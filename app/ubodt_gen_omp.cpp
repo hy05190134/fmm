d9a1f95d0927d9a25999422423ab474815ec1edc
yang.yang <yangyang09898@hellobike.com>
2019-09-04 11:31:57 +0800

    get rid of xml file as input

--- app/ubodt_gen_omp.cpp
+++ app/ubodt_gen_omp.cpp
@@ -9,44 +9,39 @@
 #include "../src/config.hpp"
 #include <iostream>
 #include <ctime>
+#include <cstdlib>
 using namespace std;
 using namespace MM;
 int main(int argc, char* argv[])
 {
     std::cout<<"------------ Fast map matching (FMM) ------------"<<endl;
-    std::cout<<"------------     Author: Can Yang    ------------"<<endl;
-    std::cout<<"------------   Version: 2018.03.09   ------------"<<endl;
+    std::cout<<"------------     Author: Can Yang, Modifed: Yang Yang    ------------"<<endl;
+    std::cout<<"------------   Version: 2019.09.04   ------------"<<endl;
     std::cout<<"------------Application: ubodt_gen_omp------------"<<endl;
-    if (argc<2) {
-        std::cout<<"No configuration file supplied"<<endl;
-        std::cout<<"A configuration file is given in the example folder"<<endl;
-        std::cout<<"Run `ubodt_gen_opt config.xml`"<<endl;
+    if (argc<4) {
+        std::cout<<"argument number error"<<endl;
+        std::cout<<"Run `ubodt_gen_opt network_file_path result_file_path delta`"<<endl;
     } else {
+        std::string network_file = argv[1];
+        std::string result_file = argv[2];
+        double delta = atof(argv[3]);
         //clock_t begin_time = clock(); // program start time
         std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
-        std::string configfile(argv[1]);
-        UBODT_Config config(configfile);
-        if (!config.validate())
-        {
-            std::cout<<"Invalid configuration file, program stop"<<endl;
-            return 0;
-        };
-        config.print();
-        std::cout<<"Write UBODT to file "<<config.result_file<<'\n';
-        MM::Network network(config.network_file,
-            config.network_id,
-            config.network_source,
-            config.network_target);
+        std::cout<<"Write UBODT to file "<<result_file<<'\n';
+        MM::Network network(network_file,
+            "id",
+            "source",
+            "target");
         // std::ostream result_file(config.result_file);
         // std::ofstream ofs(config.result_file);
         MM::NetworkGraphOmp graph(&network);
-        std::cout<<"Upperbound config (delta): "<<config.delta<<'\n';
-        bool binary = (config.binary_flag==1);
+        std::cout<<"Upperbound config (delta): "<<delta<<'\n';
+        bool binary = false;
 //         if (binary){
 //             std::cout << "---- Error: parallel construction of binary UBODT is not implemented" << '\n';
 //             return 0;
 //         }
-        graph.precompute_ubodt(config.result_file, config.delta, binary);
+        graph.precompute_ubodt(result_file, delta, binary);
         std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
         // Unit is second
         // std::cout << "Time takes" <<  <<std::endl;
