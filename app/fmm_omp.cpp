cf6053ce9a45d4880669b6564d5f05441893034e
yang.yang <yangyang09898@hellobike.com>
2019-09-04 15:15:47 +0800

    get rid of fmm xml as input

--- app/fmm_omp.cpp
+++ app/fmm_omp.cpp
@@ -34,41 +34,45 @@ int main (int argc, char **argv)
     std::cout << "------------     Author: Can Yang    ------------" << endl;
     std::cout << "------------   Version: 2018.03.09   ------------" << endl;
     std::cout << "------------   Applicaton: fmm_omp   ------------" << endl;
-    if (argc < 2)
+    if (argc < 9)
     {
-        std::cout << "No configuration file supplied" << endl;
-        std::cout << "A configuration file is given in the example folder" << endl;
-        std::cout << "Run `fmm config.xml`" << endl;
+        std::cout << "argument number error" << endl;
+        std::cout << "Run `fmm network_file_path ubodt_file_path gps_file_path result_file_path k radius gps_error penalty_factor`" << endl;
     } else {
-        std::string configfile(argv[1]);
-        FMM_Config config(configfile);
-        if (!config.validate_mm())
-        {
-            std::cout << "Invalid configuration file, program stop" << endl;
-            return 0;
-        };
-        config.print();
+        std::string network_file = argv[1];
+        std::string ubodt_file = argv[2];
+        std::string gps_file = argv[3];
+        std::string result_file = argv[4];
+        double k = atof(argv[5]);
+        double radius = atof(argv[6]);
+        double gps_error = atof(argv[7]);
+        double penalty_factor = atof(argv[8]);
 
         std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
         // std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::nanoseconds> (end - begin).count() <<std::endl;
         // clock_t begin_time = clock(); // program start time
-        Network network(config.network_file, config.network_id, config.network_source, config.network_target);
+        Network network(network_file, "id", "source", "target");
         network.build_rtree_index();
         int multiplier = network.get_node_count();
         if (multiplier==0) multiplier = 50000;
-        UBODT *ubodt=nullptr;
-        if (config.binary_flag==1){
-            ubodt = read_ubodt_binary(config.ubodt_file,multiplier);
-        } else {
-            ubodt = read_ubodt_csv(config.ubodt_file,multiplier);
-        }
-        if (!config.delta_defined){
-            config.delta = ubodt->get_delta();
-            std::cout<<"    Delta inferred from ubodt as "<< config.delta <<'\n';
-        }
-        TrajectoryReader tr_reader(config.gps_file, config.gps_id);
-        ResultConfig result_config = config.get_result_config();
-        ResultWriter rw(config.result_file,&network,result_config);
+        UBODT *ubodt = read_ubodt_csv(ubodt_file,multiplier);
+        double delta = ubodt->get_delta();
+        std::cout<<"    Delta inferred from ubodt as "<< delta <<'\n';
+
+        TrajectoryReader tr_reader(gps_file, "id");
+        ResultConfig result_config;
+        result_config.write_ogeom = true;
+        result_config.write_opath = true;
+        result_config.write_pgeom = true;
+        result_config.write_offset = true;
+        result_config.write_error = true;
+        result_config.write_spdist = true;
+        result_config.write_cpath = true;
+        result_config.write_mgeom = true;
+        result_config.write_tpath = true;
+
+
+        ResultWriter rw(result_file,&network,result_config);
         int progress = 0;
         int points_matched = 0;
         int total_points = 0;
@@ -88,10 +92,10 @@ int main (int argc, char **argv)
                 DEBUG(1) std::cout << "\n=============================" << '\n';
                 DEBUG(1) std::cout << "Process trips with id : " << trajectory.id << '\n';
                 // Candidate search
-                Traj_Candidates traj_candidates = network.search_tr_cs_knn(trajectory, config.k, config.radius,config.gps_error);
-                TransitionGraph tg = TransitionGraph(&traj_candidates,trajectory.geom, ubodt,config.delta);
+                Traj_Candidates traj_candidates = network.search_tr_cs_knn(trajectory, k, radius,gps_error);
+                TransitionGraph tg = TransitionGraph(&traj_candidates,trajectory.geom, ubodt,delta);
                 // Optimal path inference
-                O_Path *o_path_ptr = tg.viterbi(config.penalty_factor);
+                O_Path *o_path_ptr = tg.viterbi(penalty_factor);
                 T_Path *t_path_ptr = ubodt->construct_traversed_path(o_path_ptr);
                 if (result_config.write_mgeom) {
                     LineString *m_geom = network.complete_path_to_geometry(o_path_ptr,&(t_path_ptr->cpath));
